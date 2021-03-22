264487ab73ad4124cf1a3ec5b21bdb39129a375d
Arnab Das Personal <arnabd2006@gmail.com>
2021-03-22 10:58:45 +0100

    Commit 2

--- src/main/java/das/tech/helloworld/controllers/HelloWorldController.java
+++ src/main/java/das/tech/helloworld/controllers/HelloWorldController.java
@@ -1,19 +1,21 @@
 package das.tech.helloworld.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
 @Component
 @RestController
+@Slf4j
 public class HelloWorldController {
 
     @GetMapping(value = "getHelloWorld")
-    public ResponseEntity getResponse(){
+    public ResponseEntity<String> getResponse(){
+        log.info("This is it");
         return ResponseEntity.ok("Hello World");
     }
 }
